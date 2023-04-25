88025d9fd79da493db7f950141946099ac1bfccb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-24 21:06:23 -0600

    feat: Add method for active discount.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -14,4 +14,10 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def active_discount
+    item.merchant.bulk_discounts.where("#{self.quantity} >= bulk_discounts.quantity_threshold")
+    .order("bulk_discounts.discount_percent DESC")
+    .first
+  end
 end
