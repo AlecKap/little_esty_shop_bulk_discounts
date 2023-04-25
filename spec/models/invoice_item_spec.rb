52d54152fbeeed414d07bd5da06408a9ae673919
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-24 21:05:56 -0600

    feat: Add method for active discount.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -35,8 +35,28 @@ RSpec.describe InvoiceItem, type: :model do
       @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 2)
       @ii_4 = InvoiceItem.create!(invoice_id: @i3.id, item_id: @item_3.id, quantity: 1, unit_price: 5, status: 1)
     end
+
     it 'incomplete_invoices' do
       expect(InvoiceItem.incomplete_invoices).to eq([@i1, @i3])
+    end
+  end
+
+  describe 'active_discount' do
+    it 'returns a bulk_discount object if invoice_item meets threshold' do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @item_2 = Item.create!(name: "chips", description: "crunchy good", unit_price: 5, merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 15, unit_price: 10, status: 2)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 10, unit_price: 10, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 5, unit_price: 10, status: 1)
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 1)
+      @bulk_disc1 = BulkDiscount.create!(name: "Super Savings Discount", discount_percent: 0.25, quantity_threshold: 10, merchant_id: @merchant1.id)
+      @bulk_disc2 = BulkDiscount.create!(name: "Savings Discount", discount_percent: 0.20, quantity_threshold: 10, merchant_id: @merchant1.id)
+  
+      expect(@ii_1.active_discount).to eq(@bulk_disc1)
     end
   end
 end
