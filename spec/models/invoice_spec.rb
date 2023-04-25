c49879e3409eb8e9da35103a611d073e2d206c5c
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-24 19:28:13 -0600

    test: Add test for total_revenue and total_discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -14,9 +14,10 @@ RSpec.describe Invoice, type: :model do
   end
 
   describe "instance methods" do
-    it 'returns total discounted revenue of an invoice' do
-      @merchant1 = Merchant.create!(name: 'Hair Care')
-      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+    describe 'total_discounted_invoice_rev' do
+      it 'returns total discounted revenue of an invoice' do
+        @merchant1 = Merchant.create!(name: 'Hair Care')
+        @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
       @item_2 = Item.create!(name: "chips", description: "crunchy good", unit_price: 5, merchant_id: @merchant1.id)
       @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
@@ -27,11 +28,12 @@ RSpec.describe Invoice, type: :model do
       @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 1)
       @bulk_disc1 = BulkDiscount.create!(name: "Super Savings Discount", discount_percent: 0.25, quantity_threshold: 10, merchant_id: @merchant1.id)
       @bulk_disc2 = BulkDiscount.create!(name: "Super Savings Discount", discount_percent: 0.20, quantity_threshold: 10, merchant_id: @merchant1.id)
-
+      
       expect(@invoice_1.total_discounted_invoice_rev(@merchant1.id)).to eq(247.5)
     end
-
-    it "total_revenue" do
+  end
+describe 'total revenue'
+    it "returns the total invoice revenue without discounts" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
       @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
