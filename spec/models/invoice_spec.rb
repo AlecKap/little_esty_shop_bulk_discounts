1ff6aeadd07c777055d2b5781840cf5ceb541a92
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-04-24 19:26:45 -0600

    test: Add test for total_revenue and total_discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,9 +9,28 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to :customer }
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many(:merchants).through(:items) }
+    it { should have_many(:bulk_discounts).through(:merchants) }
     it { should have_many :transactions}
   end
+
   describe "instance methods" do
+    it 'returns total discounted revenue of an invoice' do
+      @merchant1 = Merchant.create!(name: 'Hair Care')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @item_2 = Item.create!(name: "chips", description: "crunchy good", unit_price: 5, merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 15, unit_price: 10, status: 2)
+      @ii_2 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 10, unit_price: 10, status: 1)
+      @ii_3 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_2.id, quantity: 5, unit_price: 10, status: 1)
+      @ii_4 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 1, unit_price: 10, status: 1)
+      @bulk_disc1 = BulkDiscount.create!(name: "Super Savings Discount", discount_percent: 0.25, quantity_threshold: 10, merchant_id: @merchant1.id)
+      @bulk_disc2 = BulkDiscount.create!(name: "Super Savings Discount", discount_percent: 0.20, quantity_threshold: 10, merchant_id: @merchant1.id)
+
+      expect(@invoice_1.total_discounted_invoice_rev(@merchant1.id)).to eq(247.5)
+    end
+
     it "total_revenue" do
       @merchant1 = Merchant.create!(name: 'Hair Care')
       @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
